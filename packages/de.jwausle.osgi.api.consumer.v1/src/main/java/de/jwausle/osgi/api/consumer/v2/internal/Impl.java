84fd2123f3908ea8116453629d9f26b5d3fd5567
Jan Winter <winter@jwausle.de>
2022-05-12 08:12:17 +0200

    Minor std classpath sample changes

--- packages/de.jwausle.osgi.api.consumer.v1/src/main/java/de/jwausle/osgi/api/consumer/v2/internal/Impl.java
+++ packages/de.jwausle.osgi.api.consumer.v1/src/main/java/de/jwausle/osgi/api/consumer/v2/internal/Impl.java
@@ -3,12 +3,12 @@ package de.jwausle.osgi.api.consumer.v2.internal;
 import de.jwausle.osgi.api.provider.Api;
 
 public class Impl implements Api{
-
-	public void v2Call() {
-		System.out.println("V2 called");
-	}
-
+	
 	public void v1Call() {
-		System.out.println("V2 called");
+		System.out.println("V1 called");
+	}
+	
+	public void v2Call() {
+		System.out.println("V1 called");
 	}
 }
---
+++ sample/run-v1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# for jar in $(find . | grep target | grep jar$ | egrep '(consumer|provider)') ; do cp $jar sample ; done
+# bash sample/run-v2.sh
+
+CLASSPATH=$(dirname $0)/de.jwausle.osgi.api.consumer.v1.jar\
+:$(dirname $0)/de.jwausle.osgi.api.consumer.v2.jar\
+:$(dirname $0)/de.jwausle.osgi.api.provider.v1.jar\
+:$(dirname $0)/de.jwausle.osgi.api.provider.v2.jar
+
+java -cp $CLASSPATH de.jwausle.osgi.api.consumer.v2.internal.Main
